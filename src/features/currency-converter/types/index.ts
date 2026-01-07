ac5bd943b95c25dba770772fa00ff86693bb66af
lucasezequielpereyra <lucaspereyra.dev@gmail.com>
2026-01-06 22:20:28 -0300

    [ UPDATE ] Add ConversionData type definition

--- src/features/currency-converter/types/index.ts
+++ src/features/currency-converter/types/index.ts
@@ -2,3 +2,12 @@ export interface Currency {
   label: string
   value: string
 }
+
+export interface ConversionData {
+  amount: number
+  fromCurrency: string
+  toCurrency: string
+  convertedAmount: number
+  rate: number
+  date: string
+}
